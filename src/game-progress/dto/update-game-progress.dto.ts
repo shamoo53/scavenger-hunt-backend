beb58c4d6520445312221f2046de068a2f8c82a4
Yusuf Falade <y-falade@leadway-group.com>
2025-05-28 11:45:42 +0100

    codebase clean up and bug fixes

--- src/game-progress/dto/update-game-progress.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateGameProgressDto } from './create-game-progress.dto';
-
-export class UpdateGameProgressDto extends PartialType(CreateGameProgressDto) {}
