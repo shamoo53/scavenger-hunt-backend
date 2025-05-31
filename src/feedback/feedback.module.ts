f964239a72d4e3cbbb832537e259fa30ce7d30af
Ibinola <ibinolamichael1@gmail.com>
2025-05-31 17:05:02 +0100

    feat: add player feedback module

---
+++ src/feedback/feedback.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Feedback } from './feedback.entity';
+import { FeedbackService } from './feedback.service';
+import { FeedbackController } from './feedback.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Feedback])],
+  providers: [FeedbackService],
+  controllers: [FeedbackController],
+})
+export class FeedbackModule {}
